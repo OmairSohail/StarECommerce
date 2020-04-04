86f5f5e5c67c11dc2dbbad5cc4c96729142f9b81
OmairSohail <halakokhan1904@gmail.com>
2020-04-04 13:17:45 +0500

    Setup Bootstrap

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="text-danger">This is an about page</h1>
+    <button class="btn btn-danger">Hello World</button>
   </div>
 </template>
